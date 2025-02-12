bc468e06c1119f84a38212f93aa30641746603d4
takabosoft <takabosoft@gmail.com>
2025-02-12 16:57:03 +0900

    MIDI入力対応

--- src/components/midiInSelector.ts
+++ src/components/midiInSelector.ts
@@ -2,13 +2,13 @@ import { midiInManager } from "../midi/midiInManager";
 import { Component } from "./component";
 
 export class MidiInSelector extends Component {
-    constructor(onChange: () => void) {
+    constructor() {
         super();
         this.element = $(`<select class="midi-in-selector">`).append(
             $(`<option>`).text("MIDI IN Device").val(""),
             midiInManager.names.map(n => $(`<option>`).text(n).val(n))
         ).on("change", () => {
-            console.log(this.element.val() + "");
+            midiInManager.curDeviceName = this.element.val() + "";
         });
     }
 }
